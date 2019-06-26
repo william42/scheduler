3f72f9a4fdecaaeab681c432ff4aea8c778ab5a1
Will A Cadegan-Schlieper <wacs@covering.space>
2019-06-26 12:46:50 -0700

    Initial version of scheduler app

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Scheduler
+
+This is a project written for a code challenge for a front-end developer position.
+
+The goal was as follows:
+* Build a form that allows the user to request appointments, giving a date, start time, and end time.
+* Allow the user to request multiple appointments.
+* Do not allow the user to create appointments in the past.
+* Do not allow the user to create appointments that intersect with each other.
+
+## Description of the project
+
+To combine these requirements, as well as the desired goal of making it easy to see where there might be a conflict, I produced the following interface:
+* The user sees a list of "forms"--I put this word in quotes because, with React (and Javascript in general), unless you're doing an explicit submit, there's no real need for `<form>` elements.
+    * Specifically, each form is a component called DateSelection, using the function-based component interface.
+* The list itself is a DateList component, using class-based components.
+* Because validation requires checking the appointments against each other, it's easiest to do that in a centralized place, so I have the DateSelection components send data back up to the DateList.  The update logic is somewhat abstracted, though, so the DateSelection could be swapped out for other date picker interfaces without too much trouble.
+* The DateList has a `focus` attribute that points to an appointment; only the focused appointment can be edited.  This is so that there is an explicit "culprit" for any conflicts.  Submitting removes focus.
+* If the set of appointments is not valid, the focused appointment cannot be submitted.  Invalidity can occur because:
+    * The appointment hasn't been filled out yet,
+    * the appointment is in the past,
+    * the start time comes after the end time (this wasn't mentioned in the requirements, but such appointments don't make sense),
+    * or because two appointments overlap.
+* To allow for mistakes, there is a "Delete" button on each appointment.
+    * Unlike modification, the user can delete unfocused appointments, because this can only ever turn an invalid set of appointments valid, not the other way around.
+    * Furthermore, it seems like it would be common to have a conflict and realize you don't want the other appointment.
+    * However, it's pretty important if you delete something, so I put up a confirm dialog.
+
+## Future directions
+
+Given the limited scope of the problem, this is a limited piece of code.  Things I can think of to expand it are:
+* Create a visual depiction of the appointments using, say, [fullcalendar](https://fullcalendar.io/).  Luckily, the data model is already well-suited for this task.
+* Connect to an actual API.  This would be the bare requirement for making a production app.
+* Related to the above, persistence.  When you refresh, all the appointments go away.  In practice, we'd want to load existing appointments from an API.
+* Styling.  Because I don't have a style guide for the company requesting this challenge, I mostly used a pretty bare style, focused on separating and lining up the elements for ease of use rather than heavy aesthetic considerations.  Given a style guide and existing CSS, I would want to match this to form a consistent style.
+* Testing.  Currently, this code is effectively untested, as my familiarity with front-end testing and the related frameworks are low.  I would want to write tests that produce various overlapping and non-overlapping scenarios, and ensure that the submit button is present or absent in these circumstances.
+
+## Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app) and you can run the below scripts, inherited from create-react-app, to run the program.
 
 ## Available Scripts
 
@@ -16,6 +55,8 @@ You will also see any lint errors in the console.
 
 Launches the test runner in the interactive watch mode.<br>
 See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+Currently, the test runner only includes the default included test.  In a production version of this, I would want to add at least some tests.
 
 ### `npm run build`
 
--- src/App.js
+++ src/App.js
@@ -1,26 +1,283 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var startData = {};
+  for (let x of [1,2,3]) {
+    startData[x] = false;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DateList data={null}/>
     </div>
   );
+}
+
+function FormRow(props) {
+  return (
+    <div className="form-row">
+      <label htmlFor={props.name}>
+        {props.display}:&nbsp;
+      </label>
+      <input
+        type={props.type}
+        id={props.name}
+        name={props.name}
+        disabled={props.disabled}
+        onChange={props.onChange} />
+    </div>
+  );
+}
+
+function DateSelection(props) {
+  var data = props.data || {
+    date: null,
+    startTime: null,
+    endTime: null,
+  };
+  function handleChange(event) {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    const newData = {...data, [name]: value};
+    props.onChange(newData);
+  }
+  function handleDelete() {
+    const ok = window.confirm("Are you sure you want to delete this appointment?");
+    if (ok) {
+      props.onDelete();
+    }
+  }
+
+  var submission = '';
+  if (props.focused) {
+    if (!props.valid) {
+      submission = (
+        <div>
+          <span role="img" aria-label="Invalid appointment">❌</span> {props.invalidReason}
+        </div>
+      );
+    } else {
+      submission = (
+        <input
+          type="button"
+          value="Submit"
+          onClick={props.onSubmit}/>
+      );
+    }
+  } else {
+    submission = (
+      <input
+        type="button"
+        value="Modify"
+        onClick={props.onRefocus}/>
+    );
+  }
+
+  return (
+    <form>
+      <FormRow
+        display="Date"
+        name="date"
+        type="date"
+        value={data.date}
+        disabled={!props.focused}
+        onChange={handleChange}/>
+      <FormRow
+        display="Start Time"
+        name="startTime"
+        type="time"
+        value={data.startTime}
+        disabled={!props.focused}
+        onChange={handleChange}/>
+      <FormRow
+        display="End Time"
+        name="endTime"
+        type="time"
+        value={data.endTime}
+        disabled={!props.focused}
+        onChange={handleChange}/>
+      <div className="form-row">
+        {submission}
+        <input
+        type="button"
+        value="Delete"
+        onClick={handleDelete}/>
+      </div>
+    </form>
+  );
+}
+
+class DateList extends React.Component {
+  constructor(props) {
+    super(props);
+    if (props.data) {
+      const keys = Object.keys(props.data).map(x => parseInt(x));
+      this.state = {
+        data: props.data,
+        next_id: Math.max(...keys) + 1,
+        focus: props.focus || null,
+      }
+    } else {
+      this.state = {
+        data: {1: null},
+        next_id: 2,
+        focus: 1,
+      }
+    }
+
+    this.addNew = this.addNew.bind(this);
+    this.handleSubmission = this.handleSubmission.bind(this);
+  }
+
+  setInvalid(reason) {
+    this.valid = false;
+    this.invalidReason = reason;
+  }
+
+  checkValidity() {
+    const focus = this.state.focus;
+    // Big assumption: only edits to the current focus will make anything invalid
+    // This should hold, since only the current focus can be edited, and can't be "blurred" without being valid
+    // It *is* possible to delete unfocused items, but that can never turn a valid thing invalid
+    if (!focus) {
+      this.valid = true;
+      return;
+    }
+    const focused_appointment = this.state.data[focus];
+    if (!focused_appointment) {
+      this.setInvalid('Enter an appointment date and time.');
+      return;
+    }
+    for (let x of ['date', 'startTime', 'endTime']) {
+      if (!focused_appointment[x]) {
+        this.setInvalid('Enter an appointment date and time.');
+        return;
+      }
+    }
+    if (focused_appointment.startTime >= focused_appointment.endTime) {
+      this.setInvalid('Appointment must end after it starts.');
+      return;
+    }
+    const now = new Date();
+    const start = new Date(focused_appointment.date + " " + focused_appointment.startTime);
+    if (now >= start) {
+      this.setInvalid('Appointment must be scheduled for after the current time.');
+      return;
+    }
+
+    for (let key in this.state.data) {
+      const other_appointment = this.state.data[key];
+      if (parseInt(key) === parseInt(focus)) {
+        continue;
+      }
+      if (focused_appointment.date !== other_appointment.date) {
+        continue;
+      }
+      // The overlap check is a bit of a trick, so I'll explain it a bit here.
+      // If one starts after the other ends then it's completely after.
+      // If one starts at the same time as the other ends we have an edge case; whether there's overlap is debatable.
+      // Otherwise, there's overlap, because appointment A is not completely before and not completely after appointment B.
+      if (
+        focused_appointment.startTime < other_appointment.endTime &&
+        other_appointment.startTime < focused_appointment.endTime
+      ) {
+        this.setInvalid("This appointment conflicts with another appointment.");
+        return;
+      }
+    }
+
+    this.valid = true;
+  }
+
+  handleDataUpdate(id) {
+    return (newData) => {
+      this.setState((state, props) => {
+        const fullNewData = {...state.data, [id]: newData};
+        return {data: fullNewData};
+      });
+    };
+  }
+
+  handleRefocus(id) {
+    return () => {
+      this.setState({focus: id});
+    }
+  }
+
+  handleDelete(id) {
+    return () => {
+      this.setState((state, props) => {
+        var newData = {...state.data}; // make a copy
+        delete newData[id];
+        var update = {data: newData};
+        if (parseInt(state.focus) === parseInt(id)) {
+          update.focus = null;
+        }
+        return update;
+      });
+    };
+  }
+
+  handleSubmission() {
+    this.checkValidity();
+    if (this.valid) {
+      this.setState({focus: null});
+    }
+  }
+
+  addNew() {
+    console.log(this.state);
+    this.setState((state, props) => {
+      const next_id = state.next_id;
+      const newData = {...state.data, [next_id]: null};
+      return {
+        data: newData,
+        next_id: next_id + 1,
+        focus: next_id,
+      };
+    });
+  }
+
+  render() {
+    const listHelper = (entry) => {
+      const [id, data] = entry;
+      return (
+        <li key={id}>
+          <DateSelection
+            data={data}
+            onChange={this.handleDataUpdate(id)}
+            onSubmit={this.handleSubmission}
+            onRefocus={this.handleRefocus(id)}
+            onDelete={this.handleDelete(id)}
+            focused={parseInt(this.state.focus) === parseInt(id)}
+            focusable={this.state.focus === null}
+            valid={this.valid}
+            invalidReason={this.invalidReason}/>
+        </li>
+      )
+    }
+
+    this.checkValidity();
+    const entries = Object.entries(this.state.data).map(listHelper);
+
+    return (
+      <ul className="date-list">
+        {entries}
+        <li key="new">
+          <input
+            type="button"
+            value="Add New"
+            onClick={this.addNew}
+            disabled = {this.state.focus !== null}
+          />
+        </li>
+      </ul>
+    );
+  }
+
 }
 
 export default App;
